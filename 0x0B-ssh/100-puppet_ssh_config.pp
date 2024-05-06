6c7c76fd26b5dd325580c01bbc7b540762049190
Zeinu Kedir <zeinuka641@gmail.com>
2024-05-06 08:06:53 -0700

    0x0B-ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,19 @@
-# Seting up my client config file
-include stdlib
+# Setting up my client SSH configuration
+include 'stdlib'  # Use single quotes for string literals
 
-file_line { 'Turn off passwd auth':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config',
-  line   => '    PasswordAuthentication no',
+# Disable password authentication
+file_line { 'Turn off password auth':  # Provide a descriptive name
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => 'PasswordAuthentication no',
+  match   => /^\s*PasswordAuthentication\s+/  # Match existing line before replacing
   replace => true,
 }
 
-file_line { 'Delare identity file':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config',
-  line   => '     IdentityFile ~/.ssh/school',
+# Set identity file
+file_line { 'Declare identity file': # Provide a descriptive name
+  ensure  => present,  # Remove indentation here
+  path    => '/etc/ssh/ssh_config',
+  line    => 'IdentityFile ~/.ssh/school',
   replace => true,
 }
